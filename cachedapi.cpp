d785b3580156517096dd26d96ba546b55fba750b
Tim Besard <tim.besard@gmail.com>
2010-10-21 21:46:07 +0200

    CachedAPI changes (way better now, I think this is gonna be the final design).

--- cachedapi.cpp
+++ cachedapi.cpp
@@ -4,8 +4,6 @@
 
 // Includes
 #include "cachedapi.h"
-#include "api/exception/interruptedexception.h"
-#include <QApplication>
 
 // Namespaces
 using namespace iRail;
@@ -17,115 +15,37 @@ using namespace iRail;
 
 CachedAPI::CachedAPI(const QString& iClientID, const QString& iClientVersion, Storage* iStorage) : AsyncAPI(iClientID, iClientVersion), mStorage(iStorage)
 {
-    mStatus = IDLE;
-
-    // Connect the signal handler
-    // As we privately inherit, we can safely assume no user will tamper with the
-    // raw API, so we can connect everything now already
-    connect(this, SIGNAL(replyStations(QList<StationPointer>)), this, SLOT(receiveStations(QList<StationPointer>)));
-    connect(this, SIGNAL(replyConnections(QList<ConnectionPointer>)), this, SLOT(receiveConnections(QList<ConnectionPointer>)));
 }
 
 
 //
-// Getters
+// Request slots
 //
 
-QList<StationPointer> CachedAPI::stations() throw(Exception)
+void CachedAPI::requestStations()
 {
-    // Try the cache
-    QList<StationPointer>* tStations = mStorage->stations();
-    if (tStations != 0)
-        return *tStations;
-
-    // Request a new object
-    AsyncAPI::requestStations();
-
-    // Wait and return
-    wait();
-    return mStations;
-}
-
-QList<ConnectionPointer> CachedAPI::connections(ConnectionRequestPointer iConnectionRequest) throw(Exception)
-{
-    // TODO: cache
-
-    // Request a new object
-    AsyncAPI::requestConnections(iConnectionRequest);
-
-    // Wait and return
-    wait();
-    return mConnections;
-}
-
-
-//
-// API response handlers
-//
-
-void CachedAPI::receiveStations(QList<StationPointer> iStations)
-{
-    if (mStatus == BUSY)
+    // Check the cache
+    QList<StationPointer>* tCachedStations = mStorage->stations();
+    if (tCachedStations != 0)
     {
-        mStorage->setStations(iStations);
-        mStations = iStations;  // TODO: pointer?
-        mStatus = IDLE;
-    }
-    else
-    {
-        //warning("received unrequested data, is somebody tampering with the underlying API?");
-    }
-}
-
-void CachedAPI::receiveConnections(QList<ConnectionPointer> iConnections)
-{
-    if (mStatus == BUSY)
-    {
-        // TODO: cache
-        mConnections = iConnections;  // TODO: pointer?
-        mStatus = IDLE;
-    }
-    else
-    {
-        //warning("received unrequested data, is somebody tampering with the underlying API?");
-    }
-}
-
-
-//
-// Auxiliary
-//
-
-void CachedAPI::wait() throw(Exception)
-{
-    // Set busy status
-    mStatus = BUSY;
-
-    // Wait for the result
-    while (mStatus == BUSY && !AsyncAPI::hasError())
-    {
-        // Perform event loop iteration
-        QApplication::processEvents();
-    }
-
-    // Problems...
-    if (mStatus == INTERRUPTED)
-    {
-        mStatus = IDLE;
-        throw new InterruptedException();
-    }
-    else if (AsyncAPI::hasError())
-    {
-        mStatus = IDLE;
-        throw AsyncAPI::error();
-    }
-    else if (mStatus == IDLE)
-    {
+        emit replyStations(*tCachedStations);
         return;
     }
-    else
-    {
-        mStatus = IDLE;
-        throw Exception("unknown problem");
-    }
+
+    // Request a new list
+    emit progress_start();
+    connect(this, SIGNAL(replyStations(QList<StationPointer>)), this, SLOT(cacheStations(QList<StationPointer>)));
+    AsyncAPI::requestStations();
+
+}
+
+
+//
+// Caching slots
+//
+
+void CachedAPI::cacheStations(QList<StationPointer> iStations)
+{
+    mStorage->setStations(iStations);
+    disconnect(this, SIGNAL(replyStations(QList<StationPointer>)), this, SLOT(cacheStations(QList<StationPointer>)));
 }
--- cachedapi.h
+++ cachedapi.h
@@ -7,61 +7,34 @@
 #define CACHEDAPI_H
 
 // Includes
-#include <QList>
-#include "api/station.h"
-#include "api/connectionrequest.h"
-#include "api/connection.h"
-#include "api/storage.h"
-#include "api/exception.h"
 #include "asyncapi.h"
-
-// TODO: document how asyncapi uses the hasError/error idiom, while the
-// blocking cachedapi can use exceptions
+#include "api/storage.h"
+#include <QString>
 
 namespace iRail
 {
     class CachedAPI : public AsyncAPI
     {
     Q_OBJECT
-        // Auxiliary types
-        enum Status
-        {
-            IDLE,
-            BUSY,
-            INTERRUPTED
-        };
-
         // Construction and destruction
     public:
         CachedAPI(const QString& iClientID, const QString& iClientVersion, Storage* iStorage);
 
-        // API response handlers
-    private slots:
-        void receiveStations(QList<StationPointer> iStations);
-        void receiveConnections(QList<ConnectionPointer> iConnections);
-
-        // Slots
+        // Request slots
     public slots:
-        //void cancel(); // TODO 1: handle this, and TODO 2: slot cancel() also in async, problem?
+        void requestStations();
 
-        // Getters
-    public:
-        QList<StationPointer> stations() throw(Exception);
-        QList<ConnectionPointer> connections(ConnectionRequestPointer iConnectionRequest) throw(Exception);
+        // Caching slots
+    private slots:
+        void cacheStations(QList<StationPointer> iStations);
+
+        // Progress indicator signals
+    signals:
+        void progress_start();
 
         // Member data
     private:
         Storage* mStorage;
-        Status mStatus;
-
-        // Temporary data
-    private:
-        QList<StationPointer> mStations;
-        QList<ConnectionPointer> mConnections;
-
-        // Auxiliary methods
-    private:
-        void wait() throw(Exception);
     };
 }
 
